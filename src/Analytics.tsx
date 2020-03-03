f258ae4643c8fa006744f0228d2e46e13bab0a2a
Kevin Deisz <kevin.deisz@gmail.com>
2020-03-03 10:05:15 -0500

    Fill out analytics shell

--- src/Analytics.tsx
+++ src/Analytics.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { Panel } from "@culturehq/components";
+import { Grid, Panel } from "@culturehq/components";
 
 const Analytics: React.FC = () => (
-  <Panel>
-    <Panel.Heading>
-      Analytics
-    </Panel.Heading>
-    <Panel.Body />
-  </Panel>
+  <Grid>
+    <Grid.Item sm={6}>
+      <Panel>
+        <Panel.Heading>
+          Total Events
+        </Panel.Heading>
+        <Panel.Body />
+      </Panel>
+    </Grid.Item>
+    <Grid.Item sm={6}>
+      <Panel>
+        <Panel.Heading>
+          Total RSVPs
+        </Panel.Heading>
+        <Panel.Body />
+      </Panel>
+    </Grid.Item>
+    <Grid.Item>
+      <Panel>
+        <Panel.Heading>
+          RSVPs by Department
+        </Panel.Heading>
+        <Panel.Body />
+      </Panel>
+    </Grid.Item>
+  </Grid>
 );
 
 export default Analytics;
