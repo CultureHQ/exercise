7fee0e47902e6c8ceda7b8cf2954416598b9b04b
Kevin Deisz <kevin.deisz@gmail.com>
2020-03-03 10:53:38 -0500

    Split up app components

--- src/app.tsx
+++ src/app.tsx
@@ -14,34 +14,42 @@ import NotFound from "./NotFound";
 import User from "./User";
 import Users from "./Users";
 
+const AppNav: React.FC = () => (
+  <Nav className="nav">
+    <NavLink exact to="/">
+      <Icon icon="home" /> Home
+    </NavLink>
+    <NavLink to="/analytics">
+      <Icon icon="analytics" /> Analytics
+    </NavLink>
+    <NavLink to="/events">
+      <Icon icon="calendar-filled" /> Events
+    </NavLink>
+    <NavLink to="/users">
+      <Icon icon="ios-people" /> Users
+    </NavLink>
+  </Nav>
+);
+
+const AppSwitch: React.FC = () => (
+  <div className="content">
+    <Switch>
+      <Route path="/analytics" component={Analytics} />
+      <Route exact path="/events" component={Events} />
+      <Route path="/events/:eventId" component={Event} />
+      <Route exact path="/users" component={Users} />
+      <Route path="/users/:userId" component={User} />
+      <Route exact path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
+);
+
 const App: React.FC = () => (
   <StrictMode>
     <Router>
-      <Nav className="nav">
-        <NavLink exact to="/">
-          <Icon icon="home" /> Home
-        </NavLink>
-        <NavLink to="/analytics">
-          <Icon icon="analytics" /> Analytics
-        </NavLink>
-        <NavLink to="/events">
-          <Icon icon="calendar-filled" /> Events
-        </NavLink>
-        <NavLink to="/users">
-          <Icon icon="ios-people" /> Users
-        </NavLink>
-      </Nav>
-      <div className="content">
-        <Switch>
-          <Route path="/analytics" component={Analytics} />
-          <Route exact path="/events" component={Events} />
-          <Route path="/events/:eventId" component={Event} />
-          <Route exact path="/users" component={Users} />
-          <Route path="/users/:userId" component={User} />
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
+      <AppNav />
+      <AppSwitch />
     </Router>
   </StrictMode>
 );
