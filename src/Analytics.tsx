41f12bdbdc0f799da949316032c69d69d3f33fc1
Kevin Deisz <kevin.deisz@gmail.com>
2020-03-03 10:52:01 -0500

    Split up analytics components

--- src/Analytics.tsx
+++ src/Analytics.tsx
@@ -1,31 +1,43 @@
 import React from "react";
 import { Grid, Panel } from "@culturehq/components";
 
+const TotalEvents: React.FC = () => (
+  <Panel>
+    <Panel.Heading>
+      Total Events
+    </Panel.Heading>
+    <Panel.Body />
+  </Panel>
+);
+
+const TotalRSVPs: React.FC = () => (
+  <Panel>
+    <Panel.Heading>
+      Total RSVPs
+    </Panel.Heading>
+    <Panel.Body />
+  </Panel>
+);
+
+const RSVPsByDepartment: React.FC = () => (
+  <Panel>
+    <Panel.Heading>
+      RSVPs by Department
+    </Panel.Heading>
+    <Panel.Body />
+  </Panel>
+);
+
 const Analytics: React.FC = () => (
   <Grid>
     <Grid.Item sm={6}>
-      <Panel>
-        <Panel.Heading>
-          Total Events
-        </Panel.Heading>
-        <Panel.Body />
-      </Panel>
+      <TotalEvents />
     </Grid.Item>
     <Grid.Item sm={6}>
-      <Panel>
-        <Panel.Heading>
-          Total RSVPs
-        </Panel.Heading>
-        <Panel.Body />
-      </Panel>
+      <TotalRSVPs />
     </Grid.Item>
     <Grid.Item>
-      <Panel>
-        <Panel.Heading>
-          RSVPs by Department
-        </Panel.Heading>
-        <Panel.Body />
-      </Panel>
+      <RSVPsByDepartment />
     </Grid.Item>
   </Grid>
 );
